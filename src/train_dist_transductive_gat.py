e5bb038513100c4cf7bdd0262e370ccee3744471
Odeinjul <odeinjul@gmail.com>
2023-12-02 04:23:09 +0800

    a

--- src/train_dist_transductive_gat.py
+++ src/train_dist_transductive_gat.py
@@ -198,7 +198,6 @@ def run(args, device, data, group=None):
                 batch_pred = model(blocks, batch_inputs)
                 if th.distributed.get_rank() == 0:
                     print(batch_pred)
-                batch_labels = batch_labels.float()
                 loss = loss_fcn(batch_pred, batch_labels)
                 th.cuda.synchronize()
                 forward_time += time.time() - forward_start
