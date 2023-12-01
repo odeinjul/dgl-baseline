82ba711604fceb915a41748b90912d37eb154cba
Odeinjul <odeinjul@gmail.com>
2023-12-02 05:19:24 +0800

    a

---
+++ gat_scripts/products_2p.sh
@@ -0,0 +1,10 @@
+python3 ~/workspace/dgl-baseline/tools/launch.py --workspace ~/workspace/dgl-baseline/src \
+    --num_trainers 8 \
+    --num_servers 1 \
+    --num_samplers 1 \
+    --part_config /home/ubuntu/workspace/data/products_2p/ogbn-products.json  \
+    --ip_config ip_config_2p.txt \
+    "/home/ubuntu/anaconda3/envs/dglbase/bin/python3 ~/workspace/dgl-baseline/src/train_dist_transductive_gat.py --graph_name ogbn-products \
+    --ip_config ip_config_2p.txt --batch_size 1000 --num_gpus 8 --eval_every 1 --num_epochs 20 \
+    --fan_out "5,10,15" --num_layers 3 --lr 0.003 --sparse_lr 0.01\
+    --dgl_sparse --num_hidden 256 --heads 8"
