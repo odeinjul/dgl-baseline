5b2ad406f890da5196391799ebcd00f20de36673
odeinjul <odeinjul@gmail.com>
2024-02-29 15:34:30 +0000

    update old result

---
+++ test.sh
@@ -0,0 +1,4 @@
+bash /home/ubuntu/workspace/dgl-baseline/gat_scripts/products_1p.sh
+bash /home/ubuntu/workspace/dgl-baseline/gat_scripts/reddit_4p.sh
+bash /home/ubuntu/workspace/dgl-baseline/gat_scripts/reddit_2p.sh
+bash /home/ubuntu/workspace/dgl-baseline/gat_scripts/reddit_1p.sh
